5c2afbc4e61ca8b893b3531b8c909f72c156428d
huanz <260540233@qq.com>
2015-03-03 13:50:46 +0800

    first commit

---
+++ seajs-cache.js
@@ -0,0 +1,40 @@
+(function () {
+    function globalEval(data) {
+        if (data && /\S/.test(data)) {
+            (window.execScript || function (data) {
+                window['eval'].call(window, data)
+            })(data)
+        }
+    }
+    function cacheJs(){
+        var exec = seajs.Module.prototype.exec;
+        seajs.Module.prototype.exec = function () {
+            if(this.uri && this.factory && !/\.css(?:\?|$)/i.test(this.uri)) {
+                localStorage.setItem('jscache<' + this.uri + '>', 'define('+this.factory.toString()+');');
+            }
+            return exec.apply(this, Array.prototype.slice.call(arguments));
+        }
+        seajs.on('request', function (request) {
+            var url = request.requestUri;
+            if(/\.css(?:\?|$)/i.test(url)) {
+                return
+            }
+            var jsCode = localStorage.getItem('jscache<' + url + '>');
+            if(jsCode && jsCode.length > 0) {
+                globalEval(jsCode);
+                request.requested = true;
+                request.onRequest();
+            }else{
+                request.requested = false;
+            }
+        });
+    }
+
+    if(!window.localStorage || seajs.data.debug){
+        console.log('浏览器不支持localStorage或开启了seajs debug模式，不缓存');
+    }else{
+        cacheJs();
+    }
+    
+
+})();
