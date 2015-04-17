2a08d7b1774e5a0b9dd112dd4c7fa6c69b8f1ffa
huanz <260540233@qq.com>
2015-04-17 17:47:04 +0800

    update

--- seajs-cache.js
+++ seajs-cache.js
@@ -1,17 +1,18 @@
-;(function () {
-	var doc = document,
-		h = doc.head,
-		store = window.localStorage;
+;(function() {
+    var doc = document,
+        h = doc.head,
+        store = window.localStorage;
+
     function globalEval(data) {
         if (data && /\S/.test(data)) {
             var script = doc.createElement('script'),
-            	code = '!function(){' + data + '\n}();';
-			script.appendChild(doc.createTextNode(code));
-			h.appendChild(script);
+                code = '!function(){' + data + '\n}();';
+            script.appendChild(doc.createTextNode(code));
+            h.appendChild(script);
         }
     }
 
-    function parseUrl(url){
+    function parseUrl(url) {
         var urlReg = /^((\w+):\/\/)?((\w+):?(\w+)?@)?([^\/\?:]+):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(\w*)/,
             fields = urlReg.exec(url),
             version = /v=\d+/.exec(fields[9]);
@@ -19,38 +20,39 @@
         return [fields[8], version];
     }
 
-    function cacheJs(){
+    function cacheJs() {
         var exec = seajs.Module.prototype.exec,
-        	slice = Array.prototype.slice;
-        seajs.Module.prototype.exec = function () {
-            if(this.uri && this.factory && !/\.css(?:\?|$)/i.test(this.uri)) {
-                store.setItem('cache~' + parseUrl(this.uri)[0], 'define("'+this.id+'", ["'+ this.dependencies.join('","')+'"], '+this.factory.toString()+');');
+            slice = Array.prototype.slice;
+        seajs.Module.prototype.exec = function() {
+            if (this.uri && this.factory && !/\.css(?:\?|$)/i.test(this.uri)) {
+                store.setItem('cache~' + parseUrl(this.uri)[0], 'define("' + this.id + '", ["' + this.dependencies.join('","') + '"], ' + this.factory.toString() + ');');
             }
             return exec.apply(this, slice.call(arguments));
         };
-        seajs.on('request', function (request) {
-            
-            var url = request.requestUri; 
-            if(/\.css(?:\?|$)/i.test(url)) {
+        seajs.on('request', function(request) {
+
+            var url = request.requestUri;
+            if (/\.css(?:\?|$)/i.test(url)) {
                 return
             }
             var pathVersion = parseUrl(url),
                 versionMap = JSON.parse(store.getItem('cache~version')) || {},
                 curVersion = pathVersion[1],
                 cacheKey = pathVersion[0],
-                jsCode = store.getItem('cache~' + cacheKey );
-            if(jsCode && jsCode.length && curVersion == versionMap[cacheKey]){
+                jsCode = store.getItem('cache~' + cacheKey);
+            if (jsCode && jsCode.length && curVersion == versionMap[cacheKey]) {
                 globalEval(jsCode);
                 request.requested = true;
                 request.onRequest();
-            }else{
+            } else {
                 versionMap[cacheKey] = curVersion;
                 store.setItem('cache~version', JSON.stringify(versionMap));
                 request.requested = false;
             }
         });
     }
-    function storageHandler(e){
+
+    function storageHandler(e) {
         var e = e || window.event;
         store.removeItem(e.key);
     }
@@ -58,14 +60,14 @@
     try {
         store.setItem('~', 1);
         store.removeItem('~');
-        if(seajs.data.debug){
+        if (seajs.data.debug) {
             console.log('开启了seajs debug模式，不缓存');
-        }else{
+        } else {
             cacheJs();
             window.addEventListener ? window.addEventListener('storage', storageHandler, false) : window.attachEvent('storage', storageHandler);
         }
-    } catch(err){
+    } catch (err) {
         console.log('浏览器不支持localStorage');
     }
-    
+
 })();
